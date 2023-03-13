b221592c43f8db8f31c9ab64b5aaf9a64c4c9adf
Alejandro Cortes <ale.crts1996@gmail.com>
2023-03-13 02:56:42 -0600

    Clean up and minor testing
    I did a CSS and NextJs components cleanup, setup most of the config file for tailwind and did some minor testing on how stuff works with both frameworks.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,31 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'black': '#505150',
+      'darkslategray': '#2f4f4f',
+      'inverted-black': '#afaeaf',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+    },
+    extend: {
+      spacing: {
+        '1w': '5vw',
+        '2w': '10vw',
+        '3w': '15vw',
+        '4w': '20vw',
+        '5w': '25vw',
+        '1h': '5vh',
+        '2h': '10vh',
+        '3h': '15vh',
+        '4h': '20vh',
+        '5h': '25vh',
+      },
+      backgroundImage: {
+        'space-image': "url('../public/pexels-instawalli-176851.jpg')",
+      }
+    },
   },
   plugins: [],
 }
